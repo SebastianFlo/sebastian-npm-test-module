5286326630d1ca20d77292a66762c62cdbb216d1
Sebastian Florian <rebardesigndk@gmail.com>
2016-02-09 15:21:13 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    // Send the Http Header
+    // Http Status : 200 : OK
+    // Content Type: text/plain
+    res.writeHead(200, { 'Content-Type' : 'text/plain' });
+    
+    // Send the response body as "Hello World"
+    res.end('Hello World');
+}).listen(8081);
+
+// Console will print the message:
+console.log('Server running at localhost : 8081')
