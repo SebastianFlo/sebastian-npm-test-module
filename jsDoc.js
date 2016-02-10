a3dc96f2ec87fbf3c4f313e9711ba53fe8f31c45
Sebastian Florian <rebardesigndk@gmail.com>
2016-02-10 17:11:15 +0100

    feature(package.json): Add jsDoc

---
+++ jsDoc.js
@@ -0,0 +1,21 @@
+// Place comments immediately before code
+
+// starts with
+/** This is the description of the foo function */
+function foo(){
+    
+}
+
+/** Tags can be added (see tag dictionary)
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+*/
+function Book(title, author){
+    
+}
+
+/** Generating a website 
+ * ./jsdoc book.js
+ * Creates a folder names "out" in the current directory.
+*/
